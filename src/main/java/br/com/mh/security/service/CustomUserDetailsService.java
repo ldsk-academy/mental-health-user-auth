29348b06f8380c4b706c96f01e85612d0db8fbf5
Vinicius Frias <vmofrias@gmail.com>
2024-10-20 18:36:17 -0300

    feat: implementação de um UserDetailsService customizado

---
+++ src/main/java/br/com/mh/security/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package br.com.mh.security.service;
+
+import br.com.mh.model.AuthUser;
+import br.com.mh.service.AuthUserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final AuthUserService authUserService;
+
+    public CustomUserDetailsService(AuthUserService authUserService) {
+
+        this.authUserService = authUserService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return authUserService.getAuthUserByNomeUsuario(AuthUser.builder().nomeUsuario(username).build());
+    }
+}
