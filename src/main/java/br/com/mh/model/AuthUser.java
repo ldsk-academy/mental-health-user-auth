0c069d60006de89c6ec2e5ae51e10927e058899f
Vinicius Frias <vmofrias@gmail.com>
2024-10-20 12:40:20 -0300

    chore: limpeza de imports

--- src/main/java/br/com/mh/model/AuthUser.java
+++ src/main/java/br/com/mh/model/AuthUser.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @Builder
