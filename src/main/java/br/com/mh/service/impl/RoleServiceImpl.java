c54d9425ad739b9460662154a64a2cb5f1bac030
Vinicius Frias <vmofrias@gmail.com>
2024-10-20 23:41:39 -0300

    fix: mensagem de erro lançada por nova exception customizada criada

--- src/main/java/br/com/mh/service/impl/RoleServiceImpl.java
+++ src/main/java/br/com/mh/service/impl/RoleServiceImpl.java
@@ -1,15 +1,19 @@
 package br.com.mh.service.impl;
 
+import br.com.mh.exception.AuthException;
 import br.com.mh.model.Role;
 import br.com.mh.repository.RoleRepository;
 import br.com.mh.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    private static final String ROLE_NOT_FOUND_MESSAGE = "Role %s não foi encontrada.";
 
     private final RoleRepository roleRepository;
 
@@ -20,10 +24,12 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
-    public Role getRoleByNome(Role role) {
+    public Role getRoleByNome(String nome) {
 
-        Optional<Role> roleOptional = roleRepository.findByNome(role.getNome());
+        Optional<Role> roleOptional = roleRepository.findByNome(nome);
 
-        return roleOptional.orElse(null);
+        return roleOptional
+                .orElseThrow(() -> new AuthException(HttpStatus.INTERNAL_SERVER_ERROR,
+                        String.format(ROLE_NOT_FOUND_MESSAGE, nome)));
     }
 }
